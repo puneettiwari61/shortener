63d4102f05208c801a873d808981d037b1a724da
Puneet Tiwari <puneettiwari61@gmail.com>
2021-01-29 23:29:34 +0530

    react-setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-Rails.start()
-ActiveStorage.start()
+var ReactRailsUJS = require("react_ujs");
+Rails.start();
+ActiveStorage.start();
+
+
+var componentRequireContext = require.context("src", true);
+ReactRailsUJS.useContext(componentRequireContext);
+
